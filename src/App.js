4db718022725fb56a21daa92f867a398f5bd8fda
Teresa Pelinski <teresapelinski@gmail.com>
2021-12-30 19:20:16 +0100

    basic intro

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-
+import './App.scss';
 export const App = () => {
 
     return (
         <main>
-            tpr
-        </main>
+            <h1>+ teresapelinski +</h1>
+            <span>
+                -- phd researcher in artificial intelligence and music (aim cdt)
+                | augmented instruments lab -- < br />
+                -- centre for digital music (c4dm) | queen mary university of london --
+            </span>
+        </main >
     )
 }
