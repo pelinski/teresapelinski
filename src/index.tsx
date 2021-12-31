2aef6d642fc86b34cb2dfb5bdc2a701e5903d02d
lewiswolf <lewiswolstanholme@gmail.com>
2021-12-31 00:46:49 +0000

    added react.strictmode have fun wit your robust and well maintained code

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
+import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
 
 document.addEventListener('DOMContentLoaded', () => {
-	const root = document.getElementById('root')
-	ReactDOM.render(<App />, root)
+	ReactDOM.render(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>,
+		document.getElementById('root')
+	)
 })
