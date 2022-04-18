4d8f20cf3f7cdcaf58c302bea8825ff814ea3d99
Teresa Pelinski <teresapelinski@gmail.com>
2022-04-18 01:01:02 +0100

    sick recursive nav

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -3,77 +3,23 @@ import React from 'react'
 export const HomePage: React.FC = (): JSX.Element => {
 	return (
 		<>
+			{' '}
 			<nav>
-				<a onClick={scrollHandler('title')}>{'teresa pelinski'}</a>
-				&nbsp;|
-				<br />
-				phd––master––––––––––––sound
-				<span>
-					<Nbsp />|<Nbsp n={4} />|
-					<br />
-					<Nbsp />|<Nbsp n={4} />|
-					<br />
-					<Nbsp />|<Nbsp n={4} />|<Nbsp n={13} />
-					+––––{'>'} <a>makam</a>
-					<br />
-					<Nbsp />|<Nbsp n={4} />|<Nbsp n={13} />|
-					<br />
-					<Nbsp />|<Nbsp n={4} />
-					+-––––––-{'>'} <a>mini projects</a> +-–––-{'>'} <a>wekitunenator</a>
-					<br />
-					<Nbsp />|<Nbsp n={20} />|
-					<br />
-					<Nbsp />|<Nbsp n={20} />|
-					<br />
-					<Nbsp />|<Nbsp n={20} />
-					+-–––-{'>'} <a>drum transcription</a>
-					<br />
-					<Nbsp />|
-					<br />
-					<Nbsp />|
-					<br />
-					<Nbsp />
-					|
-					<br />
-					<Nbsp />|
-					<br />
-					<Nbsp />
-					+––-{'>'} <a onClick={scrollHandler('about-me')}>talks </a>
-					<br />
-					&emsp;&emsp;|
-				</span>
-				{/* </span> */}
-				{/* <span className='nav-projects'>
-						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_|
-						<br />
-						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|
-						<br />
-						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|
-						<br />
-						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+––-{'>'}
-						<a>+videos+</a>
-						<br />
-						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|
-						<br />
-						<span>
-							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+––––{'>'}{' '}
-							<a onClick={scrollHandler('no-shell')}>+no-shell</a>
-							<br />
-							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|
-							<br />
-							<a>+videos+</a>
-						</span>
-					</span>
-				</span>
-				<span>
-					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|
-					<br />
-					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|
-					<br />{' '}
-					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+---
-					{'>'}
-					<a onClick={scrollHandler('infilling')}>--infilling-+</a>
-				</span> */}
+				<NavTree id={'teresa pelinski'} classN='title'>
+					<NavTree id={<a onClick={scrollHandler('about-me')}>about-me</a>} prompt={'o'} />
+					<NavTree id={'phd in ai and music _21-..25'} prompt={'+'}>
+						<NavTree id={'embedded ai workshop'} prompt={<>|&nbsp;&nbsp;{'|-->'}</>} />
+						<NavTree id={'not much to show here yet...'} prompt={<>|&nbsp;&nbsp;{'|-->'}</>} />
+					</NavTree>
+					<NavTree id={'sound and music computing master _20-21_'} prompt={'+'}>
+						<NavTree id={'master thesis'} prompt={<>|&nbsp;&nbsp;{'|-->'}</>} />
+						<NavTree id={'miniprojects'} prompt={<>|&nbsp;&nbsp;{'|-->'}</>} />
+					</NavTree>
+					<NavTree id={'sound art _??-..??_'} prompt={'+'}>
+						<NavTree id={'noshell'} prompt={<>|&nbsp;&nbsp;{'|-->'}</>} />
+					</NavTree>
+					<NavTree id={'<----––––end-––––--->'} />
+				</NavTree>
 			</nav>
 			<main>
 				<div className='screen' id='title'>
@@ -137,10 +83,39 @@ export const HomePage: React.FC = (): JSX.Element => {
 
 const scrollHandler = (scrollTo: string) => (): void => document.getElementById(scrollTo)?.scrollIntoView({ behavior: 'smooth' })
 
-const Nbsp: React.FC<{ n?: number }> = ({ n = 0 }): JSX.Element => {
-	const nbsp: JSX.Element[] = []
-	for (let i = 0; i < n; i++) {
-		nbsp.push(<>&nbsp;</>)
-	}
-	return <>{nbsp}</>
-}
+const NavTree: React.FC<{ id: string | JSX.Element; children?: JSX.Element[] | JSX.Element; prompt?: string | JSX.Element; classN?: string }> = ({
+	id,
+	children,
+	prompt,
+	classN = '',
+}): JSX.Element => (
+	<>
+		{console.log(children)}
+		<span className={classN} onClick={(classN === 'title' && scrollHandler('title')) || (() => null)}>
+			{prompt}
+			{prompt && <>&nbsp;</>}
+			{id}
+		</span>
+		{children &&
+			React.Children.map(
+				children,
+				(
+					leave,
+					index // works for both one children and an array of children
+				) => (
+					<>
+						<div className='leave' key={index}>
+							{leave}
+						</div>
+					</>
+				)
+			)}
+	</>
+)
+
+// const NavLeave: React.FC<{ children: JSX.Element; prompt: string }> = ({ children, prompt }) => (
+// 	<div className='leave'>
+// 		{prompt}&nbsp;
+// 		{children}
+// 	</div>
+// )
