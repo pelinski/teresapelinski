79a5170f7b9f9e35ae62410df5861a6d095acaca
Teresa Pelinski <teresapelinski@gmail.com>
2022-05-10 19:47:14 +0100

    cool nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import './App.scss'
 
-import { HomePage } from './pages/Home.page'
+import { PageWrapper } from './pages/PageWrapper'
 
 export const App: React.FC = (): JSX.Element => (
 	<Router>
 		<Routes>
-			<Route path='/' element={<HomePage />} />
+			<Route path='/' element={<PageWrapper screen={'title'} />} />
+			<Route path='/about-me' element={<PageWrapper screen={'about-me'} />} />
+			<Route path='/no-shell' element={<PageWrapper screen={'no-shell'} />} />
 			<Route path='*' element={<Navigate to='/' replace />} />
 		</Routes>
 	</Router>
---
+++ src/components/Title.tsx
@@ -0,0 +1,48 @@
+export const Title: React.FC = (): JSX.Element => (
+	<span>
+		hi i'm teresa pelinski and this is my website
+		<br />
+		hi i'm teresa pelinski and this is my website
+		<br />
+		hi i'm teresa pelinski and this is my website
+		<br />
+		hi i'm teresa pelinski and this is my website
+		<br />
+		hi i'm teresa pelinski and this is my website
+		<br />
+		hi i'm teresa pelinski and this is my website
+		<br />
+		hi i'm teresa pelinski and this is my website
+		<br />
+		hi i'm teresa pelinski and this is my website
+		<br />
+		hi i'm teresa pelinski and this is my website
+		<br />
+		hi i'm teresa pelinski and this is my website
+		<br />
+		hi i'm teresa pelinski and this is my website
+		<br />
+		hi i'm teresa pelinski and this is my website
+		<br />
+		hi i'm <span>teresa pelinski</span> and this is my website
+		<br />
+		hi i'm teresa pelinski and this is my <span>website</span>
+		<br />
+		hi i'm teresa pelinski and this is my website
+		<br />
+		hi i'm teresa pelinski and this is my website
+		<br />
+		hi i'm teresa pelinski and this is my website
+		<br />
+		hi i'm teresa pelinski and this is my website
+		<br />
+		hi i'm teresa pelinski and this is my website
+		<br />
+		hi i'm teresa pelinski and this is my website
+		<br />
+		hi i'm teresa pelinski and this is my website
+		<br />
+		hi i'm teresa pelinski and this is my website
+		<br />
+	</span>
+)
