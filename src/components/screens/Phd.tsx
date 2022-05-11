fb51c0d29f6de51df646fe0e5a23a93723d14acc
Teresa Pelinski <teresapelinski@gmail.com>
2022-05-11 19:24:06 +0100

    finished desktop version

--- src/components/screens/Phd.tsx
+++ src/components/screens/Phd.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import { Screen } from '../Screen'
 
-export const Phd: React.FC = () => (
-	<>
-		<span className='screen-title'>my phd</span>
+export const PhdScreen: React.FC = () => (
+	<Screen id='phd'>
+	<span className='screen-title'>my phd</span>
 		<div>
 			in september 2021 i started my phd in artificial intelligence and music (<a href='https://www.aim.qmul.ac.uk/'>aim</a> cdt) at the centre for digital music (
 			<a href='http://c4dm.eecs.qmul.ac.uk/'>c4dm</a>) at queen mary university of london (<a href='https://www.qmul.ac.uk/'>qmul</a>), where i am also part of the
-			<a href='http://instrumentslab.org/index.html'> augmented instruments lab</a>. my research is supported by <a href="https://bela.io/">bela</a> and ukri <a href="https://www.ukri.org/">ukri</a>.
+			<a href='http://instrumentslab.org/index.html'> augmented instruments lab</a>. my research is supported by <a href="https://bela.io/">bela</a> and <a href="https://www.ukri.org/">ukri</a>.
 		</div>
 		<div>
-			<span>topic</span>: sensor mesh as performance interface
+			<span className='h-yellow'>topic</span>: <span className='h-red'>sensor mesh as performance interface</span>
 			<br />
 			abstract --> despite steady advances in computational modelling of acoustic instrument sounds, digital instruments still lag far behind their traditional counterparts in the nuance of
 			interaction. in typical implementations, even the most sophisticated digital instrumental models are restricted to a small number of audio inputs and outputs, or they might be controlled
@@ -21,7 +22,7 @@ export const Phd: React.FC = () => (
 			a mesh of sensors and to evaluate the musical utility of the techniques through a new digital musical instrument.
 		</div>
 		<div>
-			<span>things i'm doing in the context of my phd</span>: <br />
+			<span className='h-yellow'>things i'm doing in the context of my phd</span>: <br />
 			-- co-organising the <a href="https://embedded-ai-for-nime.github.io/">embedded ai workshop</a> at the <a href="https://nime2022.org/">nime 2022</a> conference
             <br/>
             -- presenting my research at the <a href="https://nime2022.org/">nime 2022</a> doctoral consortium
@@ -29,5 +30,5 @@ export const Phd: React.FC = () => (
 			-- being the <a href="https://haid2022.qmul.ac.uk/">haid 2022</a> concert chair
 
 		</div>
-	</>
+		</Screen>
 )
