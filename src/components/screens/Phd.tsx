9a1858644da972a6ef51cca5e164f0dd2435007f
Teresa Pelinski <teresapelinski@gmail.com>
2023-03-19 01:15:22 +0000

    updated talks and publications

--- src/components/screens/Phd.tsx
+++ src/components/screens/Phd.tsx
@@ -11,13 +11,23 @@ export const Phd: React.FC = () => (
 			abstract --> despite steady advances in computational modelling of acoustic instrument sounds, digital instruments still lag far behind their traditional counterparts in the nuance of interaction. in typical implementations, even the most sophisticated digital instrumental models are restricted to a small number of audio inputs and outputs, or they might be controlled by the industry-standard MIDI protocol from 1983. it is thus possible to create realistic purely digital simulations of familiar instruments, but a performer's ability to play them lags far behind the acoustic original. this project investigates nuanced, high-bandwidth interaction with instruments through a mesh of sensors spread across the object, making the whole object a locus of interaction like on an acoustic instrument. however, while this approach addresses i/o bandwidth limitations, it faces a substantial challenge in extracting meaning from the sensor signals, which will have a high degree of redundancy, with gestural information encoded through slight variations between channels. machine learning offers a variety of approaches to dimensionality reduction and feature extraction from this input space. the goal of the project will be to develop suitable machine learning techniques to reduce redundancy and noise on a mesh of sensors and to evaluate the musical utility of the techniques through a new digital musical instrument.
 		</div>
 		<div>
-			<span className='h-yellow'>things i'm doing in my phd</span>: <br />
+			<span className='h-yellow'>interviews / talks / presentations / things i'm doing in my phd</span>: <br />
 			-- 16-06-2022 being interviewed at the la casa encendida's <a href="https://radio.lacasaencendida.es/programas/the-afternet/">afternet podcast</a> (spanish) <br/>
 			-- 28-06-2022 co-organising the <a href="https://embedded-ai-for-nime.github.io/">embedded ai workshop</a> at the <a href="https://nime2022.org/">nime 2022</a> conference<br/>
             -- 30-06-2022 presenting my research at the <a href="https://nime2022.org/">nime 2022</a> doctoral consortium<br />
 			-- 25-08-2022 being the concert chair at the 11th international workshop on haptic & audio interaction design (<a href="https://haid2022.qmul.ac.uk/">haid 2022</a>) <br />
+			-- 23-09-2023 being interviewed at the bela blog: <a href="https://blog.bela.io/bela-phd-sponsorship/">bela's first phd student: interview with teresa pelinski about ai, embedded computing and musical instruments</a> <br />
 			-- 27-09-2022 giving an invited talk on digital musical instruments at the <a href="/documents/2022-ucm-organologia.pdf"> 1st organoloy workshop</a> at the universidad complutense de madrid -- <a href="https://youtu.be/kZxsD32a7T0?t=1739">video</a> (spanish)<br />
-			-- 22-11-2022 giving a talk "<a href="https://www.uio.no/ritmo/english/news-and-events/events/workshops/2022/embodied-ai/p/teresa-pelinski.html">anomaly detection as means of sensing subtletly and nuance in musical gesture</a>"  at the <a href="https://www.uio.no/ritmo/english/news-and-events/events/workshops/2022/embodied-ai/index.html">embodied perspectives on musical ai (emai) worskhop</a>, held at the centre for interdisciplinarity in rhythm, time and motion (<a href="https://www.uio.no/ritmo/english/">ritmo</a>) at the university of oslo (norway) -- <a href="https://youtu.be/V8Vy1eWgoLU?t=7148">video</a>
-
+			-- 22-11-2022 giving a talk "<a href="https://www.uio.no/ritmo/english/news-and-events/events/workshops/2022/embodied-ai/p/teresa-pelinski.html">anomaly detection as means of sensing subtletly and nuance in musical gesture</a>"  at the <a href="https://www.uio.no/ritmo/english/news-and-events/events/workshops/2022/embodied-ai/index.html">embodied perspectives on musical ai (emai) worskhop</a>, held at the centre for interdisciplinarity in rhythm, time and motion (<a href="https://www.uio.no/ritmo/english/">ritmo</a>) at the university of oslo (norway) -- <a href="https://youtu.be/V8Vy1eWgoLU?t=7148">video</a> <br/>
+			-- 14-02-2024 giving a short talk and poster presentation  on my research at <a href="https://www.ircam.fr/">ircam</a> (paris) <br/>
+			-- 17-03-2024 giving a talk at the audio programmer youtube channel: <a href="https://www.youtube.com/live/eEqZJ41PbpQ?feature=share&t=364">"toward prototyping with neural networks on embedded hardware"</a> <br/>
 		</div>
+
+		<div>
+			<span className='h-yellow'>publications</span>: <br />
+			-- (upcoming) Pelinski, T., Díaz, R., Benito Temprano, A. L. & McPherson, A. (2023). Pipeline for recording datasets and running neural networks on the Bela embedded hardware platform. in Proceedings of the International Conference on New Interfaces for Musical Expression Mexico City, Mexico. <a href="https://github.com/pelinski/bela-dl-pipeline">repo link</a> <br/>
+			-- (upcoming) Pelinski, T., Caspe, F., McPherson, A. & Sandler, M. (2023). Computing ecosystems: Neural networks and embedded hardware platforms. In CHI2023 Workshop [WS2] - Beyond Prototyping Boards: Future Paradigms for Electronics Toolkits. Hamburg, Germany. <br/>
+	
+		</div>
+
 		</>)
